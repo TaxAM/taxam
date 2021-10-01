5119dda033e7fc057e0ee509067cd7b0e12016fb
Mateus Santos <a.mateussm1.5@gmail.com>
2021-10-01 18:42:54 -0300

    reading reads pt2

--- main.py
+++ main.py
@@ -27,15 +27,28 @@ counter = {}
 with open(args['reads_taxon_path']) as csvfile:
     reader = csv.reader(csvfile, delimiter = '\t')
     for line in reader:
-        # print(line)
+        
         taxonomies = line[:3]
+        id_read = taxonomies[0].strip()
 
         if len(line) > 3:
             taxonomies += [line[3].replace(';','').split()]
         else:
             continue
+        print(line)
+        type = 'read'
+        id_read = line[1][line[1].lower().find(type) + len(type):]
+        print(f'id_read: {id_read}')
+        taxon_pos = args['tax_level'] - 1
+        taxon = taxonomies[3][taxon_pos].strip()
+        if taxon.lower() == 'na':
+            print(taxon.lower())
+            print(taxon, end='')
+            print(' - Sem nada - ')
+            continue
+        else:
+            counter[id_read] = {taxon : 1}
+            print(taxon)
 
-        taxon_pos = args['tax_level']
-        # print(taxonomies[3])
-        taxon = taxonomies[3][taxon_pos]
-        print(taxon)
+for a, b in counter.items():
+    print(a, b)
