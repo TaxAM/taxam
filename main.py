6e82b2fb8dd28ad35e09d6273f68c4979d0f6f87
Mateus Santos <a.mateussm1.5@gmail.com>
2021-10-02 18:47:46 -0300

    reading reads pt3 + reading mapping pt1

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import sys
 import csv
 
-if len(sys.argv) != 6:
-    print("Usage: python main.py <taxonomy_level> <reads_taxonomy_file.tax> <ref_taxonomy_file.tax> <mapping_file_reads_to_ref.mma> <output_file_name.csv>")
-    print()
-    print("Taxonomy levels: 0-Kingdom, 1-Phylum, 2-Class, 3-Order, 4-Family, 5-Genus, 6-Species")
-    print()
-    print("Example: python main.py 5 reads.tax cds.tax mapping.mma output.csv")
+if len(sys.argv) != 7:
+    print("Usage: python main.py <taxonomy_level> <reads_taxonomy_file.tax> <ref_taxonomy_file.tax> <mapping_file_reads_to_ref.mma> <output_file_name.csv> <file_to_use>\n")
+    print("Taxonomy levels: 0-Kingdom, 1-Phylum, 2-Class, 3-Order, 4-Family, 5-Genus, 6-Species\n")
+    print("File to use: 0-Reads, 1-Contigs, 2-No one\n")
+    print("Example: python main.py 5 reads.tax cds.tax mapping.mma output.csv\n")
     sys.exit("System aborted")
 
 
-titles = ('tax_level', 'reads_taxon_path', 'ref_taxon_path', 'mapping_reads_ref_path', 'output_path')
+titles = ('tax_level', 'reads_taxon_path', 'ref_taxon_path', 'mapping_reads_ref_path', 'output_path', 'file_to_use')
 
 args = {}
 for key, arg in enumerate(sys.argv[1:]):
@@ -26,29 +25,51 @@ print("Loading taxonomy of reads...")
 counter = {}
 with open(args['reads_taxon_path']) as csvfile:
     reader = csv.reader(csvfile, delimiter = '\t')
-    for line in reader:
+    for taxonomies in reader:
         
-        taxonomies = line[:3]
-        id_read = taxonomies[0].strip()
-
-        if len(line) > 3:
-            taxonomies += [line[3].replace(';','').split()]
-        else:
+        if taxonomies[0] != 'C':
             continue
-        print(line)
-        type = 'read'
-        id_read = line[1][line[1].lower().find(type) + len(type):]
-        print(f'id_read: {id_read}')
+
+        id_read = taxonomies[1]
         taxon_pos = args['tax_level'] - 1
-        taxon = taxonomies[3][taxon_pos].strip()
-        if taxon.lower() == 'na':
-            print(taxon.lower())
-            print(taxon, end='')
-            print(' - Sem nada - ')
+        taxon = taxonomies[3].split()[taxon_pos].replace(';','')
+        if taxon == 'NA':
             continue
         else:
             counter[id_read] = {taxon : 1}
-            print(taxon)
 
 for a, b in counter.items():
+    print(a, b)
+
+print("Loading taxonomy of reference sequences...")
+taxon_ref = {}
+with open(args['ref_taxon_path']) as csvfile:
+    reader = csv.reader(csvfile, delimiter = '\t')
+    for taxonomies in reader:
+        
+        if taxonomies[0] != 'C':
+            continue
+
+        id_read = taxonomies[1]
+        taxon_pos = args['tax_level'] - 1
+        taxon = taxonomies[3].split()[taxon_pos].replace(';','')
+        if taxon == 'NA':
+            continue
+        else:
+            taxon_ref[id_read] = taxon
+
+for a, b in taxon_ref.items():
+    print(a, b)
+
+print("Mapping reads on references...")
+dic_map = {}
+with open(args['mapping_reads_ref_path']) as csvfile:
+    reader = csv.reader(csvfile, delimiter = '\t')
+    for map_line in reader:
+
+        id_read = map_line[0]
+        id_contig = map_line[1]
+        dic_map[id_read] = id_contig
+
+for a, b in dic_map.items():
     print(a, b)
