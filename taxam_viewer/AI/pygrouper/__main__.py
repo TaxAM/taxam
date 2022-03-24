4809e4b2e22fee108ab15cf3aca82ecdd0f7410f
MateusSantosMeuBem <a.mateussm1.5@gmail.com>
2022-03-24 13:20:56 -0300

    kmeans | transposing matrix and grouping samples

--- taxam_viewer/AI/pygrouper/__main__.py
+++ taxam_viewer/AI/pygrouper/__main__.py
@@ -2,14 +2,22 @@ from sklearn.cluster import KMeans
 import pandas as pd
 
 matrix = pd.read_csv(
-    './src/a_output_level0_tie-contig.taxam',
+    './src/saida-reino-no-one.taxam',
     delimiter = '\t',
     index_col= 'TaxAM'
 )
+print('Original Matrix:')
+print(matrix)
+
+transposed_matrix = matrix.transpose()
+
+print('\nTransposed Matrix:')
+print(transposed_matrix)
 
 model = KMeans(n_clusters=2)
 
-groups = model.fit_predict(matrix)
+groups = model.fit_predict(transposed_matrix)
 
-print(matrix)
+print('\nNumber of groups: 2.')
+print('Groups:')
 print(groups)
